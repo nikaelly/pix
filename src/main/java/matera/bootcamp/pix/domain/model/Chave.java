8b11d9971da8f8ef3731a18fab631b885c4bdf9b
Nikaelly V. Lima <nikaelly.lima@matera.com>
2021-09-29 20:11:32 -0300

    Criação da chave e tipo chave

--- src/main/java/matera/bootcamp/pix/domain/model/Chave.java
+++ src/main/java/matera/bootcamp/pix/domain/model/Chave.java
@@ -1,36 +1,30 @@
 package matera.bootcamp.pix.domain.model;
 
-import java.math.BigDecimal;
-import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 import lombok.Data;
 
 @Data
 @Entity
-public class ContaCorrente {
+public class Chave {
 
-    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
-    private Long agencia;
+    @Enumerated(EnumType.ORDINAL)
+    private TipoChave tipoChave;
 
     @Column(nullable = false)
-    private Long conta;
+    private String valor;
 
-    @Column(precision = 10, scale = 2, nullable = false)
-    private BigDecimal saldo;
-
-    @OneToMany
-    private List<Chave> chaves;
-
+    @OneToOne(optional = false)
+    private ContaCorrente contaCorrente;
 
 }
