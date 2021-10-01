477c946292a375846dda31c9d0608dc2700d8450
Nikaelly V. Lima <nikaelly.lima@matera.com>
2021-09-30 22:07:20 -0300

    Finalização de consultas

--- src/main/java/matera/bootcamp/pix/domain/repository/ChaveRepository.java
+++ src/main/java/matera/bootcamp/pix/domain/repository/ChaveRepository.java
@@ -1,9 +1,18 @@
 package matera.bootcamp.pix.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import matera.bootcamp.pix.domain.model.Chave;
 
 public interface ChaveRepository extends JpaRepository<Chave, Long> {
+
+    @Query("select c from Chave c " +
+            "    inner join ContaCorrente cc on cc.id = c.contaCorrente.id" +
+            "    join Usuario u on cc.id = u.contaCorrente.id " +
+            "    where u.id = :id")
+    List<Chave> findAllByUsuarioId(Long id);
 
 }
